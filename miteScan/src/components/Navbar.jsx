146050de2eecd1f62041ed43a15dd1e39980729e
unknown <gustavomlanna@gmail.com>
2025-04-18 14:46:40 -0300

    :lipstick: style: ajuste nos icons da navbar + estilização

--- miteScan/src/components/Navbar.jsx
+++ miteScan/src/components/Navbar.jsx
@@ -8,26 +8,32 @@ function Navbar() {
 
   return (
     <nav className="navbar">
+      <div className="item">
+      <img src="../../src/assets/images/logo-nav.png" alt="" />
+      </div>
+     <div className="item2">
       <ul>
         <li className={location.pathname === '/home' ? 'active' : ''}>
-          <Link to="/home"><FaHome /> Home</Link>
+          <Link to="/home"> <img src="../../src/assets/images/home-icon.png" alt="" style={{width: '50px'}}/></Link>
         </li>
         <li className={location.pathname === '/hives' ? 'active' : ''}>
-          <Link to="/hives"><FaPlus /> Cadastrar Colmeia</Link>
+          <Link to="/hives"><img src="../../src/assets/images/cadastrar-colmeia.png" alt="" style={{width: '50px'}}/> </Link>
         </li>
         <li className={location.pathname === '/historical' ? 'active' : ''}>
-          <Link to="/historical"><FaHistory /> Histórico</Link>
+          <Link to="/historical"><img src="../../src/assets/images/historico-analise.png" alt="" style={{width: '50px'}}/> </Link>
         </li>
         <li className={location.pathname === '/analysis' ? 'active' : ''}>
-          <Link to="/analysis"><FaSearch /> Analisar Colmeia</Link>
+          <Link to="/analysis"><img src="../../src/assets/images/analisar.png" alt="" style={{width: '50px'}}/></Link>
         </li>
         <li className={location.pathname === '/user' ? 'active' : ''}>
-          <Link to="/user"><FaUser /> Usuário</Link>
+          <Link to="/user"><img src="../../src/assets/images/usuario.png" alt="" style={{width: '50px'}}/></Link>
         </li>
         <li className={location.pathname === '/login' ? 'active' : ''}>
-          <Link to="/login"><FaSignOutAlt /> Sair</Link>
+          <Link to="/login"><img src="../../src/assets/images/logout.png" alt="" style={{width: '50px'}}/></Link>
         </li>
       </ul>
+     </div>
+      
     </nav>
   )
 }
