6a24f876535555dc7b2b14dfb44338034236d46f
unknown <gustavomlanna@gmail.com>
2025-04-18 14:48:04 -0300

    :construction: wip: ajustando itens + estilização no home

--- miteScan/src/pages/Home/Home.jsx
+++ miteScan/src/pages/Home/Home.jsx
@@ -10,43 +10,70 @@ function Home() {
     <div className="home-container">
       <div className="kpi">
         <div className="card-kpi">
-          <h3>🐝 Colmeias</h3>
-          <p>3</p>
+          <div className="title-kpi">
+            <img src="../src/assets/images/icon-hive-home.png" className="icon-kpi" />
+            <h3>Colmeias</h3>
+          </div>
+          <h2 style={{marginTop: '0px'}}>3</h2>
         </div>
         <div className="card-kpi">
-          <h3>🐝 Taxa de Varroa</h3>
-          <p>0%</p>
+        <div className="title-kpi">
+            <img src="../src/assets/images/icon-hive-home.png" className="icon-kpi" />
+            <h3> Taxa de Varroa</h3>
+        </div>
+        <h2 style={{marginTop: '0px'}}>0%</h2>
         </div>
         <div className="card-kpi">
-          <h3>🐝 Colmeias C/ Varroa</h3>
-          <p>0</p>
+        <div className="title-kpi">
+            <img src="../src/assets/images/icon-hive-home.png" className="icon-kpi" />
+            <h3>Colmeias + Varroa</h3>
+        </div>
+        <h2 style={{marginTop: '0px'}}>0</h2>
         </div>
       </div>
 
       <div className="dashboard">
-      <div className="top"><h2>SUAS COLMEIAS <b>EM TEMPO REAL!</b> 🐝</h2></div>
+      <div className="top"><p style={{fontSize: '23px', marginRight: '50%'}}>SUAS COLMEIAS <b>EM TEMPO REAL!</b> 🐝</p></div>
       <div className="card-box">
         {/* Card 1 */}
         <div className="card" onClick={() => navigate('/hives')}> 
         <img src="../src/assets/images/colmeia-home.png" alt="Colmeia 1" className="card-image" />
-          <h3>Colmeia 1</h3>
-          <p>Colmeia 1</p>
-          <p>23 | 45 %</p>
+        <div className="content-card">
+          <div className="info">
+            <h3>Colmeia 1</h3>
+            <p>23 | 45 %</p>
+          </div>
+          <div className="icon-secure">
+            <img src="../src/assets/images/secure-icon.png" style={{width: '40px'}}/>
+          </div>
+        </div>
         </div>
 
         {/* Card 2 */}
         <div className="card"><img src="../src/assets/images/colmeia-home.png" alt="Colmeia 1" className="card-image" />
-          <h3>Colmeia 2</h3>
-          <p>Colmeia 1</p>
-          <p>23 | 45 %</p>
+        <div className="content-card">
+          <div className="info">
+            <h3>Colmeia 2</h3>
+            <p>23 | 45 %</p>
+          </div>
+          <div className="icon-secure">
+            <img src="../src/assets/images/secure-icon.png" style={{width: '40px'}}/>
+          </div>
+        </div>
         </div>
 
         {/* Card 3 */}
         <div className="card">
         <img src="../src/assets/images/colmeia-home.png" alt="Colmeia 1" className="card-image" />
-          <h3>Colmeia 3</h3>
-          <p>Colmeia 1</p>
-          <p>23 | 45 %</p>
+        <div className="content-card">
+          <div className="info">
+            <h3>Colmeia 3</h3>
+            <p>23 | 45 %</p>
+          </div>
+          <div className="icon-secure">
+            <img src="../src/assets/images/secure-icon.png" style={{width: '40px'}}/>
+          </div>
+        </div>
         </div>
       </div>
       </div>
