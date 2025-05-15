61f4671da83eca96849585e298ab8b397fe0a0e6
unknown <gustavomlanna@gmail.com>
2025-05-15 16:12:10 -0300

    :wrench: fix: ajustes na tela de loading pós análise

--- miteScan/src/App.jsx
+++ miteScan/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import DefaultLayout from './components/DefaultLayout';
 import SplashScreen from '../src/pages/SplashScreen/SplashScreen';
 import LoadingScreen from './pages/LoadingScreen/LoadingScreen';
-import LoadingAnalysis from './components/analysis/LoadingAnalysis.jsx'; // ✅ nova importação
+import LoadingAnalysis from './components/analysis/LoadingAnalysis';
 import './styles/globals.css';
 
 import Login from '../src/pages/Login/Login';
@@ -38,17 +38,19 @@ function App() {
   return (
     <Router>
       <Routes>
+        {/* Páginas sem layout */}
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/loading" element={<LoadingScreen />} />
+        <Route path="/loading-analysis" element={<LoadingAnalysis />} /> {/* <- sem navbar aqui */}
 
+        {/* Páginas com Navbar */}
         <Route element={<DefaultLayout />}>
           <Route path="/home" element={<Home />} />
           <Route path="/hives" element={<Hives />} />
           <Route path="/historical" element={<Historical />} />
           <Route path="/analysis" element={<Analysis />} />
-          <Route path="/loading-analysis" element={<LoadingAnalysis />} /> {/* ✅ nova rota */}
           <Route path="/result-analysis" element={<ResultAnalysis />} />
           <Route path="/user" element={<User />} />
           <Route path="/create-hive" element={<CreateHive />} />
