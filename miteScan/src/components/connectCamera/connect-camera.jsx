97d4617704135b94cdb8784654faf07a0640b261
Vinicius Leal Alves <avieirakunzen@gmail.com>
2025-05-21 14:11:57 -0300

    update connect-camera.jsx

--- miteScan/src/components/connectCamera/connect-camera.jsx
+++ miteScan/src/components/connectCamera/connect-camera.jsx
@@ -4,30 +4,6 @@ import { useLocation, useNavigate } from 'react-router-dom'
 export default function CameraCard() {
     const location = useLocation()
     const navigate = useNavigate() // Para redirecionar para a tela de "analysis"
-    const videoRef = useRef(null)
-    const [cameraInfo, setCameraInfo] = useState(null)
-
-    useEffect(() => {
-        async function getCameraInfo() {
-            try {
-                const devices = await navigator.mediaDevices.enumerateDevices()
-                const videoDevices = devices.filter((device) => device.kind === 'videoinput')
-
-                if (videoDevices.length > 0) {
-                    setCameraInfo(videoDevices[0]) // pega a primeira câmera disponível
-
-                    const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: videoDevices[0].deviceId } })
-                    if (videoRef.current) {
-                        videoRef.current.srcObject = stream
-                    }
-                }
-            } catch (err) {
-                console.error('Erro ao acessar a câmera:', err)
-            }
-        }
-
-        getCameraInfo()
-    }, [])
 
     const handleBackToForm = () => {
         const origem = location.state?.origem
@@ -49,14 +25,17 @@ export default function CameraCard() {
     return (
         <>
             <h3 className='uppercase text-md mb-3'>Lique sua câmera e a conecte ao aplicativo para análise!</h3>
-            {cameraInfo ? (
-                <div className="bg-gray-100 rounded-xl shadow-xl w-sm mx-auto">
-                    <p className='p-3'><strong>Modelo:</strong> {cameraInfo.label || 'Câmera detectada'}</p>
-                    <video ref={videoRef} autoPlay playsInline width="400" className='rounded-xl' />
-                </div>
-            ) : (
-                <p>Nenhuma câmera conectada ou permissão negada.</p>
-            )}
+
+            <div className="bg-gray-100 rounded-xl shadow-xl w-sm mx-auto">
+                <p className='p-3'><strong>Modelo:</strong> ESP32-CAM</p>
+                <img
+                    src="http://192.168.1.77:81/stream"
+                    alt="Stream da ESP32"
+                    width="400"
+                    className="rounded-xl"
+                />
+            </div>
+
 
             {/* Botão "Finalizar" para redirecionar para a tela de "analysis" */}
             <button onClick={handleBackToForm}
