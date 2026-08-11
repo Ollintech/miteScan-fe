6ca154225e5e9f875433c16fe7ce257fa7fbf50a
Gustavo Marques | Plano Angelus <gustavo.marques@planoangelus.com.br>
2026-08-11 16:44:15 -0300

    :wrench: fix: permitir classe personalizada no buttonback

--- miteScan/src/components/buttonBack.jsx
+++ miteScan/src/components/buttonBack.jsx
@@ -1,11 +1,11 @@
 import { FaArrowLeft } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
-export default function ButtonBack({ title, redirect }) {
+export default function ButtonBack({ title, redirect, className = '' }) {
   const navigate = useNavigate();
 
   return (
-    <div className="flex items-center justify-between mb-6">
+    <div className={`flex items-center justify-between mb-6 ${className}`}>
       <div className="flex items-center gap-4 text-2xl font-bold uppercase">
         <button
           className="bg-yellow-400 hover:bg-yellow-300 rounded-lg shadow-md py-3 px-4"
@@ -13,6 +13,7 @@ export default function ButtonBack({ title, redirect }) {
         >
           <FaArrowLeft size={25} />
         </button>
+
         {title}
       </div>
     </div>
