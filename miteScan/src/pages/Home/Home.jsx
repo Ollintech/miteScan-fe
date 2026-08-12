2b9f218d0b010b6b13b47a35c2f2fdb87519679d
Gustavo Marques | Plano Angelus <gustavo.marques@planoangelus.com.br>
2026-08-12 17:55:20 -0300

    :lipstick: feat: centralização dos itens principais

--- miteScan/src/pages/Home/Home.jsx
+++ miteScan/src/pages/Home/Home.jsx
@@ -4,11 +4,14 @@ import HomeHives from '../../components/home/home-hives'
 import InfoHome from '../../components/home/dashboard-home'
 
 function Home() {
-
-
   return (
-    <div className="container-all">
-      <div className='w-full max-w-3xl px-4 sm:px-8 mb-5'>
+    <div className="container-all flex flex-col items-center w-full min-h-screen">
+      {/* 
+        - w-full: pega toda a largura da área livre
+        - max-w-4xl (ou 5xl): limita o tamanho dos cards
+        - mx-auto: força a margem automática nas duas laterais (centralização clássica)
+      */}
+      <div className="w-full max-w-4xl mx-auto px-4 sm:px-8 my-auto flex flex-col items-center">
         <InfoHome />
         <HomeHives />
       </div>
